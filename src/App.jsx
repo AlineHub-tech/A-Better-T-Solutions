38ea199cac3e20987137feb14a4536303d020570
Umugwaneza Aline <umugwanezaaline77@gmail.com>
2026-01-17 15:27:25 +0300

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import WhatsAppFloat from "./components/WhatsAppFloat";
-
+import WhatsAppFloat from "./components/WhatsAppFloat";   
+import ServiceDetailPage from './components/ServiceDetailPage';
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -12,21 +12,17 @@ import "./styles.css";
 function App() {
   return (
     <Router>
-      {/* TOP NAVBAR */}
-      <Navbar />
+       <Navbar />
 
-      {/* MAIN ROUTES */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/services/:title" element={<ServiceDetailPage />} />
       </Routes>
-
-      {/* WHATSAPP FLOAT */}
       <WhatsAppFloat />
-
-      {/* FOOTER */}
       <Footer />
+
     </Router>
   );
 }
