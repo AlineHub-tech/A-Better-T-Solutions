2136033b6d44fe97270fc3e0a494f746f4f20101
Umugwaneza Aline <umugwanezaaline77@gmail.com>
2026-01-17 19:31:55 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -54,7 +54,6 @@ export default function Footer(){
       </div>
       <div className="footer-bottom">
         <p>&copy; {currentYear} A Better-T Solutions. All Rights Reserved.</p>
-
         <div className="footer-legal">
             <a href="#">Privacy Policy</a>
             <a href="#">Terms of Service</a>
@@ -63,6 +62,7 @@ export default function Footer(){
     </footer>
   );
 }
+
 
 
 
