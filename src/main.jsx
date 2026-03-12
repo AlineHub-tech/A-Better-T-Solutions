8ef866c88efe9c1c0f6cee9bccabeb4da74a4e58
Umugwaneza Aline <umugwanezaaline77@gmail.com>
2026-03-12 20:51:36 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 // 1. Hamagara styles.css hano kugira ngo ikwire hose (Global Styles)
-import '../styles/styles.css' 
+import './app.css'  
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
